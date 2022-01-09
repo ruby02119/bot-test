af2341892f2c3a7f82c3823d7830ff079ae9a971
D4 <97005110+ruby02119@users.noreply.github.com>
2022-01-09 21:10:55 +0700

    Add files via upload

--- plugins/nhentai.js
+++ plugins/nhentai.js
@@ -1,39 +1,30 @@
-const nhentai = require('nhentai-node-api')
-const request = require('request')
-const topdf = require('image-to-pdf')
-let fetch = require('node-fetch')
 let fs = require('fs')
+let request = require('request')
+let fetch = require('node-fetch')
+let topdf = require('image-to-pdf')
+let nhentai = require('nhentai-node-api')
 
-let handler = async(m, { conn, usedPrefix, command, args }) => {
-	if(!args[0]) throw `Masukkan kode nya!\n\nContoh: ${usedPrefix + command} 257326`
-	let count = 0
-	let ResultPdf = []
-	let doujin = await nhentai.getDoujin(args[0])
-	let title = doujin.title.default
-	let native = doujin.title.native
-	let details = doujin.details
-	let array_page = doujin.pages
-	let cover = doujin.cover
-	let language = doujin.language
-	let favorites = doujin.favourites
-	let capton = `
-Doujin Downloader
-${title} ${native}
-Language: ${language}
-Parody: ${details.parodies}
-Group: ${details.groups}
-Artist: ${details.artists}
-Tag: ${details.tags}
-Category: ${details.categories}
-Favorited: ${doujin.favorites}
+let handler = async (m, { conn, args }) => {
+	if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys) return !0
+	if (m.quoted && m.quoted.fromMe && m.quoted.isBaileys && /getnhentai 1/i.test(m.quoted.text)) {
+		if (!args[0]) return m.reply('Masukkan angka')
+		if (isNaN(args[0])) return m.reply('Pake angka')
+		await m.reply('Loading...')
+		let data = JSON.stringify(await eval(`${args[0]}-1`))
+		let input = m.quoted.text.split('='.repeat(25))[data].split('ID: ')[1].split('\n')[0]
+		let count = 0
+		let ResultPdf = []
+		let doujin = await nhentai.getDoujin(input)
+		let title = doujin.title.default
+		let details = doujin.details
+		let parodies = details.parodies.map(v => v.name)
+		let characters = details.characters.map(v => v.name)
+		let tags = details.tags.map(v => v.name)
+		let artists = details.artists.map(v => v.name)
+		let groups = details.groups.map(v => v.name)
+		let categories = details.categories.map(v => v.name)
+		let array_page = doujin.pages
 
-Cara membuka Internet Positif menggunakan Chrome tanpa VPN
-https://telegra.ph/Cara-membuka-Internet-Positif-menggunakan-Chrome-12-23
-`.trim()
-	if(!args[1]) {
-	await conn.send2ButtonImg(m.chat, await(await fetch(cover)).buffer(), capton, watermark, 'Download PDF', `.${usedPrefix + command} ${args[0]} -d`, 'Read online', `.${usedPrefix + command} ${args[0]} -o`, m)
-	} else if(args[1] === '-d') {
-		m.reply('Sedang mengambil data.\nHarap tunggu sekitar 1~5 menit...')
 		for (let index = 0; index < array_page.length; index++) {
 			if (!fs.existsSync('./nhentai')) fs.mkdirSync('./nhentai')
 			let image_name = './nhentai/' + title + index + '.jpg'
@@ -42,7 +33,8 @@ https://telegra.ph/Cara-membuka-Internet-Positif-menggunakan-Chrome-12-23
 			ResultPdf.push(image_name)
 			count++
 		}
-	await new Promise((resolve) =>
+
+		await new Promise((resolve) =>
 			topdf(ResultPdf, 'A4')
 			.pipe(fs.createWriteStream('./nhentai/' + title + '.pdf'))
 			.on('finish', resolve)
@@ -51,16 +43,31 @@ https://telegra.ph/Cara-membuka-Internet-Positif-menggunakan-Chrome-12-23
 		for (let index = 0; index < array_page.length; index++) {
 			fs.unlinkSync('./nhentai/' + title + index + '.jpg')
 		}
-	m.reply('Sedang mengirim file PDF...')
-	let thumbnail = await (await fetch(doujin.cover)).buffer()
-	await conn.sendFile(m.chat, fs.readFileSync(`./nhentai/${title}.pdf`), `${title}.pdf`, '', m, false, { asDocument: true, thumbnail: thumbnail })
-		.then(() => fs.unlinkSync(`./nhentai/${title}.pdf`))
-	} else if (args[1] === '-o') {
-		m.reply(`Silahkan buka link untuk membaca secara online.\nhttps://hiken.xyz/v/${args[0]}`)
+		
+		let size = await fs.statSync(`./nhentai/${title}.pdf`).size
+		if (size < 45000000) {
+			m.reply('Uploading...')
+			let thumbnail = await (await fetch(doujin.cover)).buffer()
+			await conn.sendFile(m.chat, fs.readFileSync(`./nhentai/${title}.pdf`), `${title}.pdf`, '', m, false, { asDocument: true, thumbnail: thumbnail })
+			.then(() => fs.unlinkSync(`./nhentai/${title}.pdf`))
+		} else {
+			m.reply('Uploading to anonfiles because file size to large')
+			let options = {
+				method: 'POST',
+				url: 'https://api.anonfiles.com/upload',
+				formData: {
+					file: fs.createReadStream(`./nhentai/${title}.pdf`),
+				},
+			}
+			
+			request(options, function(err, res, body) {
+				if (err) throw err
+				fs.unlinkSync(`./nhentai/${title}.pdf`)
+				m.reply('Link download to file: ' + JSON.parse(body).data.file.url.full)
+			})
+		}
 	}
 }
 
-handler.command = /^nhentai|doujin$/i
-handler.help = ['nhentai <kode>']
-handler.tags = ['downloader']
+handler.command = /^get?(nhentai|hentai|doujin|nh)$/i
 module.exports = handler
